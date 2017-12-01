f15e8609bf12894857cb456d5a10391947e5a8c3
Gurthwath <bob.bob7888@googlemail.com>
2017-12-01 10:20:26 -0500

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,21 @@
+document.getElementById("div-for-navbar").innerHTML =
+'<nav class="navbar navbar-default navbar-top navbar-fixed-top">
+  <div class="container">
+    <a class="nav-link" href="/lojban-learning-resource/">Home</a>
+    <a class="nav-link" href="/lojban-learning-resource/about">About Lojban</a>
+    <a class="nav-link" href="/lojban-learning-resource/flashcards">Lojban Flashcards</a>
+    <a class="nav-link" href="/lojban-learning-resource/games">Lojban Games</a>
+  </div>
+</nav>
+
+<nav class="navbar navbar-default navbar-bottom navbar-fixed-bottom">
+  <div class="container">
+  <span class="nav navbar-nav navbar-left">
+    <span>Made by: Andrew Kilburn</span>
+  </span>
+  <span class="nav navbar-nav navbar-right">
+    <span><a href = "/lojban-learning-resource/documentation">Documentation</a></span> - 
+    <span><a href = "/lojban-learning-resource/sitemap">Sitemap</a></span>
+  </span>
+  </div>
+</nav>'
